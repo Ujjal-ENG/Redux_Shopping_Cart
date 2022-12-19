b9bfc4757af9bd9c606cdc44ac3431f82921bb48
user <ujjalroy7862@gmail.com>
2022-12-19 16:10:47 +0600

    now the design part is okay

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,15 +2,36 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Products = () => {
+  
   const [products, setProducts] = useState([]);
+
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const res = await
-    }
-  }, [])
-  
-  return <div>Products</div>;
+      const res = await fetch("https://fakestoreapi.com/products");
+
+      const data = await res.json();
+      console.log(data);
+
+      setProducts(data);
+    };
+    fetchProduct();
+  }, []);
+
+  return (
+    <div className="productsWrapper">
+      {products.map((product) => (
+         (
+          <div className="card" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h4>{product.title}</h4>
+            <h5>${product.price}</h5>
+            <button className="btn">Add to Cart</button>
+          </div>
+        )
+      ))}
+    </div>
+  );
 };
 
 export default Products;
