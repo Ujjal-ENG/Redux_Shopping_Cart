c74f4de63dd092587d7e7bc3b81bb2a59cc09d7a
user <ujjalroy7862@gmail.com>
2022-12-19 17:33:33 +0600

    now my reduxjs toolkit is okay

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
 import Navbar from "./components/Navbar";
-import "./App.css"
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Router>
+
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 
-const Products = () => {
-  
-  const [products, setProducts] = useState([]);
+import { add } from "../store/cartSlice";
 
+import { useDispatch } from "react-redux";
+
+const Products = () => {
+  const dispatch = useDispatch();
+
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -18,17 +22,22 @@ const Products = () => {
     fetchProduct();
   }, []);
 
+  const handleAdd = (product) => {
+    //strore the product store in redux store
+    dispatch(add(product));
+  };
+
   return (
     <div className="productsWrapper">
       {products.map((product) => (
-         (
-          <div className="card" key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <h4>{product.title}</h4>
-            <h5>${product.price}</h5>
-            <button className="btn">Add to Cart</button>
-          </div>
-        )
+        <div className="card" key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <h4>{product.title}</h4>
+          <h5>${product.price}</h5>
+          <button className="btn" onClick={() => handleAdd(product)}>
+            Add to Cart
+          </button>
+        </div>
       ))}
     </div>
   );
