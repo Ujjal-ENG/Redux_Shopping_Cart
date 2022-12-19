9cfeead07eb963401c836ef19e78d8ef1fed0b07
user <ujjalroy7862@gmail.com>
2022-12-19 18:20:56 +0600

    done cart section successfully

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  
+  const cartItems = useSelector((state) => state.cart);
 
-export default Cart
+  const dispatch = useDispatch();
+
+  const handleRemove = (id) => {
+    dispatch(remove(id));
+  };
+
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div className="cartWrapper">
+        {cartItems.map((item) => (
+          <div className="cartCard" key={item.id}>
+            <img src={item.image} alt={item.title} />
+            <h4>{item.title}</h4>
+            <h5>${item.price}</h5>
+            <button className="btn" onClick={() => handleRemove(item.id)}>
+              Remove Items
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
