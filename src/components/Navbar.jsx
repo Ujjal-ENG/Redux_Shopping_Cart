4800f4d0d0462beccc0be857364b5ccc77246bbf
user <ujjalroy7862@gmail.com>
2022-12-19 18:04:28 +0600

    now cart function finally work as i prefer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+  
+  const items = useSelector((state) => state.cart);
+
   return (
     <div>
       <div id="kopa">
@@ -13,7 +18,7 @@ const Navbar = () => {
         <Link className="navLink" to="/cart">
           Cart
         </Link>
-        <span className="cartCount">Cart Items: 0</span>
+        <span className="cartCount">Cart Items: {items.length}</span>
       </div>
     </div>
   );
